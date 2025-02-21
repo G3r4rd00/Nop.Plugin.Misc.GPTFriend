a3933e17cbc83a18fd9902c9eea251d0c924600f
gerard.tous <gerard.tous@perfumesclub.com>
2025-02-21 11:32:11 +0100

    cambios

--- obj/Debug/net7.0/Nop.Plugin.Misc.GPTFriend.AssemblyInfo.cs
+++ obj/Debug/net7.0/Nop.Plugin.Misc.GPTFriend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Nop Solutions, Ltd")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a77d7e8c34dc92f987ca26a909895f5f3be3a90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a61da8deca2ab5a41babb17ec7db3feb604baaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nop.Plugin.Misc.GPTFriend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nop.Plugin.Misc.GPTFriend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
